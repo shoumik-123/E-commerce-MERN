33b9922327cac0d59bd092cf551bcc71c19962f9
shoumik-123 <shabikyeaminshoumik123@gmail.com>
2024-01-22 05:54:44 +0600

    message

--- client/src/components/3D/View3d.jsx
+++ client/src/components/3D/View3d.jsx
@@ -1,35 +1,37 @@
-import React, {Fragment} from 'react';
+import React, { useEffect, Fragment } from 'react';
 import Shoe3D from "./Shoe3d/Shoe3D.jsx";
-import {getInfoFor3D} from "../../helper/SassionHelper.js";
-import {toast} from "react-toastify";
-import {Link, useNavigate} from "react-router-dom";
+import { getInfoFor3D } from "../../helper/SassionHelper.js";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const View3D = () => {
+    const navigate = useNavigate();
 
-    const navigate =  useNavigate()
+    useEffect(() => {
+        const subcategory = getInfoFor3D().subcategory;
+
+        if (subcategory === "Laptop") {
+            toast.info("Laptop 3D model will be imported as soon as possible");
+        } else if (subcategory === "T-Shirt") {
+            toast.info("T-Shirt 3D model will be imported as soon as possible");
+        } else if (subcategory === "Mobile") {
+            toast.info("Mobile 3D model will be imported as soon as possible");
+        } else if (subcategory === "Guitar") {
+            toast.info("Guitar 3D model will be imported as soon as possible");
+        }
+    }, []);
+
     const goBack = () => {
-      navigate(-1)
-    }
+        navigate(-1);
+    };
+
     return (
         <Fragment>
             <div className="body3D">
-                {getInfoFor3D().subcategory === "Shoe" &&(
-                    <Shoe3D/>
-                )}
-                {getInfoFor3D().subcategory === "Laptop" &&(
-                    toast.info("Laptop 3D model Will be import as soon  as  possible")
-
-                )}
-                {getInfoFor3D().subcategory === "T-Shirt" &&(
-                    toast.info("T-Shirt 3D model Will be import as soon  as  possible")
-                )}
-                {getInfoFor3D().subcategory === "Mobile" &&(
-                    toast.info("Mobile 3D model Will be import as soon  as  possible")
-                )}
-                {getInfoFor3D().subcategory === "Guitar" &&(
-                    toast.info("Guitar 3D model Will be import as soon  as  possible")
-                )}
-                <button className="goBackBtnFrom3dPage" onClick={goBack}>Go Back</button>
+                {getInfoFor3D().subcategory === "Shoe" && <Shoe3D />}
+                <button className="goBackBtnFrom3dPage" onClick={goBack}>
+                    Go Back
+                </button>
             </div>
         </Fragment>
     );
